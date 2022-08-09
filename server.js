158cacc7cf3e380b21d47a5d7955b5ca1ed30fbd
CHalseth <Corey.Halseth@gmail.com>
2022-08-09 00:58:03 -0500

    make it better

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const apiRoutes = require("./routes/api.js");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,16 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb: //localhost: 27017/budget-tracker",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  }
-);
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
